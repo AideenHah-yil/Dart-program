617b4b689502febc21646e75bafc8d038af9f070
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-09 00:55:46 -0800

    initial commit

---
+++ main.dart
@@ -0,0 +1,31 @@
+// Dart program to demonstrate data types
+
+void main() {
+    // Example of int data type
+    int age = 25; //represents the age of a person
+    print('Age: $age');
+}
+
+void main() {
+    // Example of double date type
+    double height = 1.80; //represents the height of a person
+    print('Height: $height');
+}
+
+void main() {
+    //Example of String data type
+    String name = "Aideen";
+    print("Name: $name");
+}
+
+void main() {
+    // Example of a list data type
+    List<String> fruits = ['Apple', 'Banana', 'Pineapple', 'Passion', 'Grapes'];
+    print('Fruits: $fruits');
+}
+
+void main() {
+    //example of a map data type
+    Map<String, String> capitalCities = {'Japan': 'Tokyo', 'Nepal': 'Kathmandu', 'England': 'London'};
+    print('Capital cities: $capitalCities');
+}
