52c8b19d8deee48b9fddcd9f021a9d16b14817d4
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-09 01:00:44 -0800

    Fixing errors

--- main.dart
+++ main.dart
@@ -2,42 +2,22 @@
 
 void main() {
     // Example of int data type
-    int age = 25; //represents the age of a person
+    int age = 25; // Represents the age of a person
     print('Age: $age');
-}
-
-void main() {
-    // Example of double date type
-    double height = 1.80; //represents the height of a person
+    
+    // Example of double data type
+    double height = 1.80; // Represents the height of a person
     print('Height: $height');
-}
-
-void main() {
-    //Example of String data type
+    
+    // Example of String data type
     String name = "Aideen";
     print("Name: $name");
-}
-
-void main() {
-    // Example of a list data type
+    
+    // Example of a List data type
     List<String> fruits = ['Apple', 'Banana', 'Pineapple', 'Passion', 'Grapes'];
     print('Fruits: $fruits');
-}
-
-void main() {
-    //example of a map data type
+    
+    // Example of a Map data type
     Map<String, String> capitalCities = {'Japan': 'Tokyo', 'Nepal': 'Kathmandu', 'England': 'London'};
     print('Capital cities: $capitalCities');
-}
-
-void main() {
-    // Example of a set data type
-    Set<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    print('Numbers: $numbers');
-}
-
-void main() {
-    // example of a boolean type
-    bool isMarried = true;
-    print('Is married: $isMarried');
 }
