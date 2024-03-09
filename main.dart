e4877573383afa741d7acf1b4a9d13456e620d0e
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-09 00:56:49 -0800

    adding data  types

--- main.dart
+++ main.dart
@@ -29,3 +29,9 @@ void main() {
     Map<String, String> capitalCities = {'Japan': 'Tokyo', 'Nepal': 'Kathmandu', 'England': 'London'};
     print('Capital cities: $capitalCities');
 }
+
+void main() {
+    // Example of a set data type
+    Set<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    print('Numbers: $numbers');
+}
